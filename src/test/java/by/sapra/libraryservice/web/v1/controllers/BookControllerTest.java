00eb3e1f28f353032c468db467b1398c9718ce5c
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-17 14:10:32 +0300

    Создать контроллер для управления книгами. #2
    Изменён тест: теперь он не проверяет правильность возвращённых данных, но проверяет логику работы самого метода. Сделано это с учётом того, что правильность ответа будет проверяться в интеграционном тестировании.

--- src/test/java/by/sapra/libraryservice/web/v1/controllers/BookControllerTest.java
+++ src/test/java/by/sapra/libraryservice/web/v1/controllers/BookControllerTest.java
@@ -1,17 +1,23 @@
 package by.sapra.libraryservice.web.v1.controllers;
 
+import by.sapra.libraryservice.testUtils.StringTestUtils;
 import by.sapra.libraryservice.web.v1.models.WebBookFilter;
+import net.javacrumbs.jsonunit.JsonAssert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = BookController.class)
@@ -19,6 +25,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BookControllerTest {
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private WebFilterMapper webMapper;
+
+    @MockBean
+    private BookService service;
+
+    @MockBean
+    private BookResponseMapper responseMapper;
 
     @Value("${app.base-url}")
     private String baseUrl;
@@ -35,5 +50,41 @@ class BookControllerTest {
                         .param("author", filter.getAuthor())
                 )
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenFindByFilter_thenReturnResponse(Integer id) throws Exception {
+        WebBookFilter filter = new WebBookFilter();
+        filter.setAuthor("test_title");
+        filter.setTitle("test_author");
+
+        ServiceFilter serviceFilter = new ServiceFilter();
+        serviceFilter.setAuthor(filter.getAuthor());
+        serviceFilter.setTitle(filter.getTitle());
+        when(webMapper.webFilterToServiceFilter(filter)).thenReturn(serviceFilter);
+
+        BookModel book = BookModelBuilder.aBook()
+                                .withAuthor(filter.getTitle())
+                                .withTitle(filter.getAuthor())
+                                .build();
+        when(service.filterBook(serviceFilter)).thenReturn(book);
+
+
+        BookResponse response = BookResponseBuilder.aBookResponse()
+                                .withAuthor(filter.getAuthor())
+                                .withTitle(filter.getTitle())
+                                .build();
+        when(responseMapper.bookModelToResponse(book)).thenReturn(response);
+
+        mvc.perform(
+                        get(baseUrl)
+                                .param("title", filter.getTitle())
+                                .param("author", filter.getAuthor()))
+                .andReturn().getResponse()
+                .getContentAsString();
+
+        verify(webMapper, times(1)).webFilterToServiceFilter(filter);
+        verify(service, times(1)).filterBook(serviceFilter);
+        verify(responseMapper, times(1)).bookModelToResponse(book);
     }
 }
