e17aaa2f35de223e1ffe1f839708bcb880d81aa7
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-17 16:24:33 +0300

    Создать контроллер для управления книгами. #2
    В интерфейсы добавлены необходимые для поиска книги по имени категории методы.

--- src/main/java/by/sapra/libraryservice/services/BookService.java
+++ src/main/java/by/sapra/libraryservice/services/BookService.java
@@ -3,6 +3,10 @@ package by.sapra.libraryservice.services;
 import by.sapra.libraryservice.services.model.BookModel;
 import by.sapra.libraryservice.services.model.ServiceFilter;
 
+import java.util.List;
+
 public interface BookService {
     BookModel filterBook(ServiceFilter filter);
+
+    List<BookModel> getBookByCategory(String categoryName);
 }
--- src/main/java/by/sapra/libraryservice/web/v1/mappers/BookResponseMapper.java
+++ src/main/java/by/sapra/libraryservice/web/v1/mappers/BookResponseMapper.java
@@ -1,8 +1,13 @@
 package by.sapra.libraryservice.web.v1.mappers;
 
 import by.sapra.libraryservice.services.model.BookModel;
+import by.sapra.libraryservice.web.v1.models.BookListResponse;
 import by.sapra.libraryservice.web.v1.models.BookResponse;
+
+import java.util.List;
 
 public interface BookResponseMapper {
     BookResponse bookModelToResponse(BookModel bookModel);
+
+    BookListResponse bookModelListToListResponse(List<BookModel> modelList);
 }
