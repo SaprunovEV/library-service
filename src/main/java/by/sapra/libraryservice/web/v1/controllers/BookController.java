a277016e9513b92a63bb78ca9e961309614c8b3f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-17 17:02:42 +0300

    Создать контроллер для управления книгами. #2
    Создана болванка с правильным статусом, нужна лишь для прохождения теста.

--- src/main/java/by/sapra/libraryservice/web/v1/controllers/BookController.java
+++ src/main/java/by/sapra/libraryservice/web/v1/controllers/BookController.java
@@ -6,11 +6,9 @@ import by.sapra.libraryservice.web.v1.mappers.WebFilterMapper;
 import by.sapra.libraryservice.web.v1.models.CategoryName;
 import by.sapra.libraryservice.web.v1.models.WebBookFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/book")
@@ -35,5 +33,10 @@ public class BookController {
                         .bookModelListToListResponse(
                                 service.getBookByCategory(categoryName.getCategoryName()))
         );
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewBook() {
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
     }
 }
