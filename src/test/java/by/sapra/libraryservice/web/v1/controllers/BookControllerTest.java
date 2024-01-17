04e643498aaa0955283985f8e4c6362d03a949ef
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-17 11:35:07 +0300

    Создать контроллер для управления книгами. #2
    добавлена подтяжка основы адреса из файла тестовых свойств.

--- src/test/java/by/sapra/libraryservice/web/v1/controllers/BookControllerTest.java
+++ src/test/java/by/sapra/libraryservice/web/v1/controllers/BookControllerTest.java
@@ -2,18 +2,24 @@ package by.sapra.libraryservice.web.v1.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(controllers = BookController.class)
+@ActiveProfiles("web_test")
 class BookControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     @Test
     void shouldDoSomething() throws Exception {
-
+        System.out.println();
     }
 }
