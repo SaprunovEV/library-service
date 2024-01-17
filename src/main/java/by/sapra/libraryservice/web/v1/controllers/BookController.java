b749e97ff7cdd73110d24bc749bf59767ac5fbf7
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-17 15:00:12 +0300

    Создать контроллер для управления книгами. #2
    Закончен метод фильтрации книг по авторам и названиям.

--- src/main/java/by/sapra/libraryservice/web/v1/controllers/BookController.java
+++ src/main/java/by/sapra/libraryservice/web/v1/controllers/BookController.java
@@ -1,15 +1,28 @@
 package by.sapra.libraryservice.web.v1.controllers;
 
+import by.sapra.libraryservice.services.BookService;
+import by.sapra.libraryservice.web.v1.mappers.BookResponseMapper;
+import by.sapra.libraryservice.web.v1.mappers.WebFilterMapper;
 import by.sapra.libraryservice.web.v1.models.WebBookFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/book")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService service;
+    private final WebFilterMapper filterMapper;
+    private final BookResponseMapper responseMapper;
     @GetMapping
-    public void getBookByAuthorAndTitle(WebBookFilter filter) {
-        System.out.println();
+    public ResponseEntity<?> getBookByAuthorAndTitle(WebBookFilter filter) {
+        return ResponseEntity.ok(
+                responseMapper.bookModelToResponse(
+                        service.filterBook(
+                                filterMapper.webFilterToServiceFilter(filter)))
+        );
     }
 }
