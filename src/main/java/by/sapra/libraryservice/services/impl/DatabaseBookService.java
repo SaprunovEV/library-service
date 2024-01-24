30c7489cab7dd848e98ea288b927af61ba6e729b
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-24 16:39:03 +0300

    Создать сервис для бизнес логики библиотеки; #3
    добавлен тест для не найденой книги.

--- src/main/java/by/sapra/libraryservice/services/impl/DatabaseBookService.java
+++ src/main/java/by/sapra/libraryservice/services/impl/DatabaseBookService.java
@@ -1,17 +1,32 @@
 package by.sapra.libraryservice.services.impl;
 
+import by.sapra.libraryservice.models.BookEntity;
+import by.sapra.libraryservice.repository.BookRepository;
+import by.sapra.libraryservice.repository.specifications.BookSpecification;
 import by.sapra.libraryservice.services.BookService;
+import by.sapra.libraryservice.services.mappers.BookServiceMapper;
 import by.sapra.libraryservice.services.model.BookModel;
 import by.sapra.libraryservice.services.model.ServiceFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class DatabaseBookService implements BookService {
+    private final BookRepository repository;
+    private final BookServiceMapper mapper;
+
     @Override
     public BookModel filterBook(ServiceFilter filter) {
-        return null;
+        Optional<BookEntity> optional = repository.findOne(BookSpecification.withFilter(filter));
+        if (optional.isEmpty()) {
+            return null;
+        }
+
+        return mapper.entityToModel(optional.get());
     }
 
     @Override
