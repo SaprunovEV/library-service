c8870c60da371ae07e3336a2a7315bd22a1fcfec
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-24 15:25:05 +0300

    Создать сервис для бизнес логики библиотеки; #3
    добавлены зависимости на тест контейнер и базы данных

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,18 +22,18 @@ repositories {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
-//	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("net.javacrumbs.json-unit:json-unit:2.38.0")
-//	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 //	testImplementation("org.testcontainers:junit-jupiter")
-//	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:postgresql")
 }
 
 tasks.withType<Test> {
