867077a7cce6baa90d38218101babcce585ae61b
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-24 16:25:47 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Создан тест для проверки фильтрации книг.

--- src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
+++ src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
@@ -1,4 +1,8 @@
 package by.sapra.libraryservice.services.mappers;
 
+import by.sapra.libraryservice.models.BookEntity;
+import by.sapra.libraryservice.services.model.BookModel;
+
 public interface BookServiceMapper {
+    BookModel entityToModel(BookEntity entity);
 }
--- src/test/java/by/sapra/libraryservice/services/BookServiceTest.java
+++ src/test/java/by/sapra/libraryservice/services/BookServiceTest.java
@@ -1,14 +1,23 @@
 package by.sapra.libraryservice.services;
 
 import by.sapra.libraryservice.config.AbstractDataTest;
+import by.sapra.libraryservice.models.BookEntity;
 import by.sapra.libraryservice.services.mappers.BookServiceMapper;
+import by.sapra.libraryservice.services.model.BookModel;
+import by.sapra.libraryservice.services.model.ServiceFilter;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
+import static by.sapra.libraryservice.testUtils.builders.models.BookEntityTestDataBuilder.aBookEntity;
+import static by.sapra.libraryservice.testUtils.builders.service.BookModelBuilder.aBook;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = BookServiceConf.class)
@@ -19,5 +28,29 @@ class BookServiceTest extends AbstractDataTest {
     @MockBean
     private BookServiceMapper mapper;
 
+    @Test
+    void whenFilterBook_thenReturnModel() throws Exception {
+        BookEntity entity = getFacade().save(aBookEntity());
 
+        ServiceFilter filter = new ServiceFilter();
+        filter.setTitle(entity.getTitle());
+        filter.setAuthor(entity.getAuthor());
+
+        BookModel model = aBook()
+                .withId(entity.getId())
+                .withAuthor(entity.getAuthor())
+                .withTitle(entity.getTitle())
+                .build();
+        when(mapper.entityToModel(eq(entity)))
+                .thenReturn(model);
+
+        BookModel actual = service.filterBook(filter);
+
+        assertAll(() -> {
+            assertNotNull(actual);
+            assertEquals(model, actual);
+        });
+
+        verify(mapper, times(1)).entityToModel(eq(entity));
+    }
 }
