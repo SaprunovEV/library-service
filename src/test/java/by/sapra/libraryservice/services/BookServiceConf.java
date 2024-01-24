dfdfc762b9e935fc3c28977da2676124b5454366
Евгений Сапрунов <sapra10062018@gmail.com>
2024-01-24 16:37:11 +0300

    Создать сервис для бизнес логики библиотеки; #3
    В конструктор сервиса добавлены необходимые компоненты для работы.

--- src/test/java/by/sapra/libraryservice/services/BookServiceConf.java
+++ src/test/java/by/sapra/libraryservice/services/BookServiceConf.java
@@ -1,13 +1,15 @@
 package by.sapra.libraryservice.services;
 
+import by.sapra.libraryservice.repository.BookRepository;
 import by.sapra.libraryservice.services.impl.DatabaseBookService;
+import by.sapra.libraryservice.services.mappers.BookServiceMapper;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class BookServiceConf {
     @Bean
-    public BookService service() {
-        return new DatabaseBookService();
+    public BookService service(BookRepository repo, BookServiceMapper mapper) {
+        return new DatabaseBookService(repo, mapper);
     }
 }
