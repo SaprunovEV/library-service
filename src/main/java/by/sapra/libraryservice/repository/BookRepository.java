4ed0066298734b12d0f7562b8d1e032bce7dab76
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-07 15:37:10 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Добавлены методы для поиска по категории и преобразования листа сущностей в лист моделей соответственно.

--- src/main/java/by/sapra/libraryservice/repository/BookRepository.java
+++ src/main/java/by/sapra/libraryservice/repository/BookRepository.java
@@ -4,5 +4,8 @@ import by.sapra.libraryservice.models.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<BookEntity, Integer>, JpaSpecificationExecutor<BookEntity> {
+    List<BookEntity> findByCategoryEntity_Name(String name);
 }
--- src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
+++ src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
@@ -3,6 +3,10 @@ package by.sapra.libraryservice.services.mappers;
 import by.sapra.libraryservice.models.BookEntity;
 import by.sapra.libraryservice.services.model.BookModel;
 
+import java.util.List;
+
 public interface BookServiceMapper {
     BookModel entityToModel(BookEntity entity);
+
+    List<BookModel> entityListToBookModelList(List<BookEntity> books);
 }
