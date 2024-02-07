a72106f8ff6eaf0fd6e2592aae459719979f1253
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-07 14:51:09 +0300

    Создать сервис для бизнес логики библиотеки; #3
    В построитель тестовой сущности книги был добавлен код для учёта обязательной сущности категории.

--- src/test/java/by/sapra/libraryservice/testUtils/builders/models/BookEntityTestDataBuilder.java
+++ src/test/java/by/sapra/libraryservice/testUtils/builders/models/BookEntityTestDataBuilder.java
@@ -1,17 +1,21 @@
 package by.sapra.libraryservice.testUtils.builders.models;
 
 import by.sapra.libraryservice.models.BookEntity;
+import by.sapra.libraryservice.models.CategoryEntity;
 import by.sapra.libraryservice.testUtils.builders.TestDataBuilder;
 
 public class BookEntityTestDataBuilder implements TestDataBuilder<BookEntity> {
     private String title = "test_title";
     private String author = "test_author";
 
+    private TestDataBuilder<CategoryEntity> category = CategoryEntity::new;
+
     private BookEntityTestDataBuilder() {}
 
-    private BookEntityTestDataBuilder(String title, String author) {
+    private BookEntityTestDataBuilder(String title, String author, TestDataBuilder<CategoryEntity> category) {
         this.title = title;
         this.author = author;
+        this.category = category;
     }
 
     public static BookEntityTestDataBuilder aBookEntity() {
@@ -19,17 +23,24 @@ public class BookEntityTestDataBuilder implements TestDataBuilder<BookEntity> {
     }
 
     public BookEntityTestDataBuilder withAuthor(String author) {
-        return this.author == author ? this : new BookEntityTestDataBuilder(title, author);
+        return this.author == author ? this : new BookEntityTestDataBuilder(title, author, category);
     }
 
     public BookEntityTestDataBuilder withTitle(String title) {
-        return this.title == title ? this : new BookEntityTestDataBuilder(author, title);
+        return this.title == title ? this : new BookEntityTestDataBuilder(author, title, category);
+    }
+
+    public BookEntityTestDataBuilder withCategory(TestDataBuilder<CategoryEntity> category) {
+        return this.category == category ? this : new BookEntityTestDataBuilder(author, title, category);
     }
     
     
     @Override
     public BookEntity build() {
         BookEntity result = new BookEntity();
+        CategoryEntity build = category.build();
+        result.setCategoryEntity(build);
+        build.getBooks().add(result);
         result.setAuthor(author);
         result.setTitle(title);
         return result;
