714f79b35a2019506fd9751c51b7fac2b85ed166
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-14 18:13:14 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Убран не нужный бин, из-за которого падали другие тесты.

--- src/test/java/by/sapra/libraryservice/services/mappers/CategoryServiceMapperConfig.java
+++ src/test/java/by/sapra/libraryservice/services/mappers/CategoryServiceMapperConfig.java
@@ -1,14 +1,9 @@
 package by.sapra.libraryservice.services.mappers;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @TestConfiguration
 @ComponentScan(basePackages = "by.sapra.libraryservice.services.mappers")
 public class CategoryServiceMapperConfig {
-    @Bean
-    public CategoryServiceMapper mapper() {
-        return new CategoryServiceMapperDecorator();
-    }
 }
