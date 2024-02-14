10fe89c28aeb8b34dc65ac4b279a0243371eb620
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-14 16:36:22 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Добавлен mapstruct. Над методом entityToModel добавлено правило преобразования категории в её Id.

--- src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
+++ src/main/java/by/sapra/libraryservice/services/mappers/BookServiceMapper.java
@@ -2,10 +2,16 @@ package by.sapra.libraryservice.services.mappers;
 
 import by.sapra.libraryservice.models.BookEntity;
 import by.sapra.libraryservice.services.model.BookModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
+import static org.mapstruct.ReportingPolicy.IGNORE;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE)
 public interface BookServiceMapper {
+    @Mapping(source = "categoryEntity.id", target = "categoryId")
     BookModel entityToModel(BookEntity entity);
 
     List<BookModel> entityListToBookModelList(List<BookEntity> books);
