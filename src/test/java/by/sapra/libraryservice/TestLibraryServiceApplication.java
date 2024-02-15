228502a2e012176f170efeaa8232bd39c1658410
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-15 15:28:07 +0300

    Создать сервис для бизнес логики библиотеки; #3
    В контейнер для сгенерированного теста добавлена схема. Это сделано для ликвидации ошибки создания таблиц.

--- src/test/java/by/sapra/libraryservice/TestLibraryServiceApplication.java
+++ src/test/java/by/sapra/libraryservice/TestLibraryServiceApplication.java
@@ -14,7 +14,7 @@ public class TestLibraryServiceApplication {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest")).withInitScript("static/schema.sql");
     }
 
     @Bean
