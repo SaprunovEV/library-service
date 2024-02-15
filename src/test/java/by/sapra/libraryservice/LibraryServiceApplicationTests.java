95954605fdb72d8c3bec1c24a9cf41e5fd740636
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-15 14:29:18 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Починил тест сгенерированный спрингом. Теперь контекст собирается без проблем.

--- src/test/java/by/sapra/libraryservice/LibraryServiceApplicationTests.java
+++ src/test/java/by/sapra/libraryservice/LibraryServiceApplicationTests.java
@@ -2,8 +2,10 @@ package by.sapra.libraryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestLibraryServiceApplication.class)
 class LibraryServiceApplicationTests {
 
     @Test
