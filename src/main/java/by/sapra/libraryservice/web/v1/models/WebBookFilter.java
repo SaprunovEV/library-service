e4939912d549b16fa9d9f6c71143065afb8523a6
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-15 16:38:12 +0300

    Создать сервис для бизнес логики библиотеки; #3
    Добавлена валидация параметров.

--- src/main/java/by/sapra/libraryservice/web/v1/models/WebBookFilter.java
+++ src/main/java/by/sapra/libraryservice/web/v1/models/WebBookFilter.java
@@ -1,9 +1,12 @@
 package by.sapra.libraryservice.web.v1.models;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class WebBookFilter {
+    @NotBlank(message = "Поле автора не должно быть пустым!")
     private String author;
+    @NotBlank(message = "Поле титула не должно быть пустым!")
     private String title;
 }
