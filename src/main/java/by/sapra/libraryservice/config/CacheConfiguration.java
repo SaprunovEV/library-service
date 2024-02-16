f4091ffda433189b732e5d8b8bbe4f5ca3678561
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-16 13:14:27 +0300

    Добавить кешь для книг. #8Добавить кешь для книг. #8
    Создана конфигурация подключающая кешь.

---
+++ src/main/java/by/sapra/libraryservice/config/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package by.sapra.libraryservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
