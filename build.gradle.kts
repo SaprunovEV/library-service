718ad9b40511d6a5b62d2e173f866c39b4960980
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-19 15:55:33 +0300

    Добавить кешь для книг. #8Добавить кешь для книг. #8
    Добавлен процессор аннотаций для сбора свойств в классе обёртке.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,15 @@ dependencies {
 	implementation("com.google.guava:guava:32.1.1-jre")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("io.lettuce:lettuce-core")
+
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
 
 	implementation("org.mapstruct:mapstruct:1.5.3.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
