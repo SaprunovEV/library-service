617b85cfe762d38adbc3a05b7fc103456b8de900
Евгений Сапрунов <sapra10062018@gmail.com>
2024-02-26 13:43:45 +0300

    Добавить кешь для книг. #8Добавить кешь для книг. #8
    В связи с включением кеширования был изменён интеграционный тест.

--- src/test/java/by/sapra/libraryservice/integrations/WebIntegrationTest.java
+++ src/test/java/by/sapra/libraryservice/integrations/WebIntegrationTest.java
@@ -9,6 +9,7 @@ import by.sapra.libraryservice.testUtils.builders.TestDataBuilder;
 import by.sapra.libraryservice.web.v1.models.UpsertBookRequest;
 import by.sapra.libraryservice.web.v1.models.WebBookFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redis.testcontainers.RedisContainer;
 import net.javacrumbs.jsonunit.JsonAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTypeExcludeFil
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
 
 import static by.sapra.libraryservice.testUtils.builders.models.BookEntityTestDataBuilder.aBookEntity;
 import static by.sapra.libraryservice.testUtils.builders.models.CategoryEntityTestDataBuilder.aCategoryEntity;
@@ -39,7 +48,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureTestEntityManager
 @ImportAutoConfiguration
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = CacheRedisConfig.class)
+@Testcontainers
 public class WebIntegrationTest extends AbstractDataTest {
+
+    @Container
+    protected static RedisContainer redisContainer =
+            new RedisContainer(DockerImageName.parse("redis:7.0.12"))
+                    .withReuse(true)
+                    .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    public static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.redis.enable", () -> "true");
+        registry.add("app.cache.type", () -> "redis");
+        registry.add("app.cache.cache-names", () -> List.of("bookByCategoryName", "booksByAuthorAndTitle"));
+        registry.add("app.cache.caches.bookByCategoryName.expire", () -> "10m");
+        registry.add("app.cache.caches.booksByAuthorAndTitle.expire", () -> "10m");
+
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
+    }
 
     @Autowired
     private ObjectMapper objectMapper;
